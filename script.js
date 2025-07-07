8a779378c9df6b0f1643a4fc89de0125fa8793e5
Gustavo Cabral <studiogustavocabral@gmail.com>
2025-07-07 19:59:56 -0300

    test

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('copy-btn').addEventListener('click', async () => {
+  try {
+    const response = await fetch('card.base64.txt');
+    const base64Payload = await response.text();
+
+    const html = `
+      <span 
+        data-buffer="<!--(figma)${base64Payload}(/figma)-->" 
+        contenteditable="true" 
+        style="white-space:pre"
+      >
+        Figma Component
+      </span>
+    `;
+
+    const blob = new Blob([html], { type: 'text/html' });
+    const clipboardItem = new ClipboardItem({ 'text/html': blob });
+
+    await navigator.clipboard.write([clipboardItem]);
+    alert("Copiado! Agora cole no Figma com ⌘+V");
+  } catch (err) {
+    alert("Erro ao copiar: " + err);
+  }
+});
